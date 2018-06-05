4d05b616d09b60aaf2cd925174025c18a3dcb407
CestierAnthony <cestier.anthony@live.fr>
2018-06-05 08:44:31 +0200

    Add files via upload

---
+++ controller/frontend/CommentManager.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once('model/CommentManager.php');
+
+function addComment($postId, $author, $comment)
+{
+    $commentManager = new CommentManager();
+
+    $affectedLines = $commentManager->postComment($postId, $author, $comment);
+
+    if ($affectedLines === false) {
+        throw new Exception('Impossible d\'ajouter le commentaire !');
+    }
+    else {
+        header('Location: index.php?action=post&id=' . $postId);
+    }
+}
+
+function reportComment($id)
+{
+    $commentManager = new CommentManager();
+
+    $affectedLines = $commentManager->reportComment($id);
+
+    if ($affectedLines === false) {
+        throw new Exception('Impossible de signaler le commentaire !');
+    }
+    else {
+       header('Location: index.php');
+    }
+}
---
+++ controller/frontend/PostManager.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once('model/PostManager.php');
+require_once('model/CommentManager.php');
+
+function listPosts()
+{
+    $postManager = new PostManager(); // Création d'un objet
+    $posts = $postManager->getPosts(); // Appel d'une fonction de cet objet
+
+    require('view/frontend/listPostsView.php');
+}
+
+function post()
+{
+    $postManager = new PostManager();
+    $commentManager = new CommentManager();
+
+    $post = $postManager->getPost($_GET['id']);
+    $comments = $commentManager->getComments($_GET['id']);
+
+    require('view/frontend/postView.php');
+}
