7eae1fdaa5a69ab439d6bec463466d6a68095d6b
CestierAnthony <cestier.anthony@live.fr>
2018-06-21 17:44:16 +0200

    Add files via upload

--- admin.php
+++ admin.php
@@ -1,6 +1,11 @@
 <?php
-require('controller/backend/CommentAdmin.php');
-require('controller/backend/PostAdmin.php');
+
+require('controller/backend/LogIn.php');
+session_start();
+if(!empty($_SESSION['id']))
+{
+	require('controller/backend/CommentAdmin.php');
+    require('controller/backend/PostAdmin.php');
 
         if ($_GET['action'] == 'deleteComment') {
             deleteComment($_GET['id']);
@@ -11,21 +16,39 @@ require('controller/backend/PostAdmin.php');
         else if($_GET['action'] == 'deletePost') {
             deletePost($_GET['id']);
         }
-        else if($_GET['action'] == 'addPosts') {
-            addPost($_GET['id']);
+        else if($_GET['action'] == 'addPost') {
+            addpostAdmin($_GET['id']);
         }
         else if($_GET['action'] == 'postAdmin'){
-			postAdmin();
-		}
+            postAdmin();
+        }
         else if($_GET['action'] == 'editPost'){
             editpostAdmin($_GET['id']);
         }
-		else if($_GET['action'] == 'commentAdmin'){
-			commentAdmin();
-		}
+        else if($_GET['action'] == 'commentAdmin'){
+            commentAdmin();
+        }
         else if($_GET['action'] == 'changePost'){
             changepostAdmin();
         }
-		else{
-			addpostAdmin();
-		}
+        else if($_GET['action'] == 'addpostAdmin'){
+            addpostAdmin();
+        }
+         else if($_GET['action'] == 'logOut'){
+            logOut();
+        }
+        else{
+        postAdmin();
+        }
+}
+
+else{
+    if($_GET['action'] == 'logIn'){
+        logIn();
+    } else {
+        showlogIn();
+    }
+
+}
+
+  
